0ea590dcdd7c784fe8ca19cc0bc5105f9000292d
Samvel <108175853+Samvel-Hakobyan@users.noreply.github.com>
2022-08-14 19:20:27 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App(props) {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/'/>
+
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
