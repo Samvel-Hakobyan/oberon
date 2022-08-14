e80cf85680c0525930b997a487a04f1e6e2c7de9
Samvel <pochtanamber1@gmail.com>
2022-08-14 20:26:53 +0300

    init

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App(props) {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/'/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
